c534b865d3c1d9aad2a3726d45e295febe5d2475
Marco Pivetta <ocramius@gmail.com>
2012-07-24 10:27:28 +0200

    Fixing autoloading in the test suite

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,26 +1,43 @@
 <?php
+
+use Zend\Loader\StandardAutoloader;
+use DoctrineMongoODMModuleTest\BaseTest;
+
 chdir(__DIR__);
 
 $previousDir = '.';
+
 while (!file_exists('config/application.config.php')) {
     $dir = dirname(getcwd());
+
     if($previousDir === $dir) {
         throw new RuntimeException(
             'Unable to locate "config/application.config.php": ' .
-            'is DoctrineORMModule in a subdir of your application skeleton?'
+            'is DoctrineMongoODMModuleTest in a subdir of your application skeleton?'
         );
     }
+
     $previousDir = $dir;
     chdir($dir);
 }
 
-$loader = require_once('vendor/autoload.php');
-$loader->add('DoctrineMongoODMModuleTest', __DIR__);
-
-if (is_readable(__DIR__ . '/TestConfiguration.php')) {
-    require_once __DIR__ . '/TestConfiguration.php';
-} else {
-    require_once __DIR__ . '/TestConfiguration.php.dist';
+if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !(@include_once __DIR__ . '/../../../autoload.php')) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
-\DoctrineMongoODMModuleTest\BaseTest::setMvcConfig($configuration);
+if (!$config = @include __DIR__ . '/TestConfiguration.php') {
+    $config = require __DIR__ . '/TestConfiguration.php.dist';
+}
+
+$loader = new StandardAutoloader(array(
+    StandardAutoloader::LOAD_NS => array(
+        'DoctrineMongoODMModuleTest' => __DIR__ . '/DoctrineMongoODMModuleTest',
+    ),
+));
+$loader->register();
+
+if (!$config = @include __DIR__ . '/TestConfiguration.php') {
+    $config = require __DIR__ . '/TestConfiguration.php.dist';
+}
+
+BaseTest::setMvcConfig($config);
