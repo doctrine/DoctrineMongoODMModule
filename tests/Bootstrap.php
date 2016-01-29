788ab6f8f4dddceb2084d19ef9f64431746a23b1
Andreas Braun <git@alcaeus.org>
2016-01-29 19:17:19 +0100

    Install phpunit through composer

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,7 +2,6 @@
 chdir(dirname(__DIR__));
 
 $loader = require_once('vendor/autoload.php');
-$loader->add('DoctrineMongoODMModuleTest', __DIR__);
 
 $config = include(__DIR__ . '/test.application.config.php');
 
